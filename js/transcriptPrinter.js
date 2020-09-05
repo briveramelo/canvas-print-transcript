05ffd83652ab22f413101b7cb08c6b485ec21a84
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2020-09-05 00:16:17 -0600

    added method for grouping text by customizable durations, to cut out excessive timestamp printing and group by 30sec, or 60sec at your discretion. 30 is default fixed up text formatting a bit and placement

--- js/transcriptPrinter.js
+++ js/transcriptPrinter.js
@@ -1,22 +1,67 @@
-let getTranscript = function()
+let getRawTranscript = function()
 {
     let arr = [];
     let elms = $(".cielo24-vwrap-sentence");
     for (let i = 0; i < elms.length; i++) {
-        if (elms[i].innerText !== null) {
+        if (elms[i].innerText) {
             arr.push(elms[i].innerText);
         }
     }
-    return arr.join("");
+    return arr.join(" ");
 }
 
-let jsPDFUrl = "https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js";
+let getTimeAccountedTranscript = function(timeGrouping_sec, onGetTranscript)
+{
+    let momentUrl = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js";
+    $.getScript(momentUrl, function (data) {
+
+        let getTextTimeObj = function(text)
+        {
+            let timePattern = /^([0-9]{2}:[0-9]{2}:[0-9]{2})[\n]*(.*)$/g;
+            let matches = text.matchAll(timePattern);
+            let captures = matches.next().value;
+
+            if(captures && captures.length > 2)
+            {
+                return {
+                    timeStamp:moment(`01/01/2000 ${captures[1]}`),
+                    text:captures[2],
+                }
+            }
+            return null;
+        }
+
+        let arr = [];
+        let elms = $(".cielo24-vwrap-sentence");
+        let lastPrintedMoment = moment("01/01/2000 00:00:00");
+        for (let i = 0; i < elms.length; i++) {
+            let text = elms[i].innerText;
+            if (text) {
+                let textTime = getTextTimeObj(text);
+                if(textTime)
+                {
+                    if(textTime.timeStamp.diff(lastPrintedMoment, 'seconds') < timeGrouping_sec)
+                    {
+                        arr.push(textTime.text);
+                        continue;
+                    }
+                    lastPrintedMoment = textTime.timeStamp;
+                }
+                arr.push(`\n${text}`);
+            }
+        }
+
+        onGetTranscript(arr.join(" "))
+    });
+}
+
 let makePdf = function (textToPrint) {
+    let jsPDFUrl = "https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js";
     $.getScript(jsPDFUrl, function (data) {
         let doc = new jsPDF();
         doc.setFontSize(12);
         doc.setTextColor(0,0,0);
-        let defaultTextPos = {x: 15, y: 10};
+        let defaultTextPos = {x: 18, y: 18};
         let textPos = {x: defaultTextPos.x, y: defaultTextPos.y};
 
         let applyNewLine = function (height, lineCount, textPos) {
@@ -27,17 +72,15 @@ let makePdf = function (textToPrint) {
         };
         let writeLine = function (doc, line, lineHeight, textPos) {
             doc.text(textPos.x, textPos.y, line);
-            applyNewLine(lineHeight, textPos);
+            applyNewLine(lineHeight, 1, textPos);
         };
         let writeLines = function (doc, text, lineHeight, lineWidth, textPos) {
             let lines = doc.splitTextToSize(text, lineWidth);
-            console.log(text);
-            console.log(lines);
             for (let i = 0; i < lines.length; i++) {
-                // if (textPos.y > doc.internal.pageSize.height - lineHeight) {
-                //     resetTextPos(textPos);
-                //     doc.addPage();
-                // }
+                if (textPos.y > doc.internal.pageSize.height - lineHeight * 2) {
+                    resetTextPos(textPos);
+                    doc.addPage();
+                }
                 writeLine(doc, lines[i], lineHeight, textPos);
             }
         };
@@ -45,12 +88,23 @@ let makePdf = function (textToPrint) {
             textPos.x = defaultTextPos.x;
             textPos.y = defaultTextPos.y;
         }
-        // writeLines(doc, textToPrint, 12, 167, textPos);
-        let lines = doc.splitTextToSize(textToPrint, 167);
-        doc.text(15, 10, lines);
+        writeLines(doc, textToPrint, 6, 175, textPos);
         doc.save(`MGT6051_LectureVideoTranscript.pdf`);
     });
 }
-makePdf(getTranscript());
+
+// let trans = getRawTranscript();
+// if(trans)
+// {
+//     makePdf(trans);
+// }
+
+getTimeAccountedTranscript(30, (trans)=>{
+    if(trans)
+    {
+        makePdf(trans);
+    }
+});
+
 //todo: provide opt to remove timestamps
 //todo: print to .pdf file
